fdadc16d92f2a72a561135050ffebaee5f37ba19
Ilya Rypinski <iripinskij@gmail.com>
2020-06-16 16:56:07 +0300

    base application NestJS

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  //const app = await NestFactory.create(AppModule);
+  await app.listen(7000);
+}
+bootstrap();
