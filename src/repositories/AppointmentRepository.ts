35807aef525638593764612f41edb072fde2b67a
Ilya Rypinski <iripinskij@gmail.com>
2020-07-15 20:57:42 +0300

    full functionality updated

--- src/repositories/AppointmentRepository.ts
+++ src/repositories/AppointmentRepository.ts
@@ -2,37 +2,22 @@ import { Op } from 'sequelize';
 import { services } from '../database/models/services';
 import { barbers } from '../database/models/barbers';
 import { appointments } from '../database/models/appointments';
+import { log_error } from '../helpers/error-handler';
 
 export class AppointmentRepository {
-  async GetAppointment(
-    appointment_id: number,
-    client_id: number,
-  ): Promise<any> {
-    return await appointments.findOne({
-      where: {
-        id: appointment_id,
-        client_id: client_id,
-        deleted: false,
-      },
-    });
-  }
 
   async showMyAppointments(id: number): Promise<Array<any>> {
     return await appointments.findAll({
       where: {
-        client_id: id,
-        deleted: false,
+        client_id: id, deleted: false,
         date: { [Op.gte]: new Date() },
       },
       attributes: ['date', 'id'],
-      include: [
+      include: [{
+          model: barbers, attributes: ['first_name', 'last_name'],
+      },
         {
-          model: barbers,
-          attributes: ['first_name', 'last_name'],
-        },
-        {
-          model: services,
-          attributes: ['name'],
+          model: services, attributes: ['name'],
         },
       ],
     });
@@ -41,13 +26,9 @@ export class AppointmentRepository {
   async showMyHistory(id: number): Promise<Array<any>> {
     return await appointments.findAll({
       where: {
-        client_id: id,
-        deleted: false,
-        date: {
-          [Op.lte]: new Date(),
-        },
-      },
-      raw: true,
+        client_id: id, deleted: false,
+        date: { [Op.lte]: new Date(), },
+      }
     });
   }
 
@@ -56,15 +37,93 @@ export class AppointmentRepository {
     nextDay.setDate(nextDay.getDate() + 1);
     return await appointments.findAll({
       where: {
-        date: {
-          [Op.between]: [check_date, nextDay],
-        },
+        date: { [Op.between]: [check_date, nextDay] },
         deleted: false,
       },
-      raw: true,
     });
   }
 
+  async ChangeBarber(id, msg: any): Promise<any>{
+    const data = msg.split(' ');
+    const appointment = await appointments.findOne({
+      where: {
+        deleted: false,
+        client_id: id,
+        id: data[0]
+      }
+    });
+    if(data[1] = undefined)
+      data[1] = 'default';
+    await appointment.update({
+      barber_id: data[1]
+    });
+    return 'Your appointment updated';
+  }
+
+  async ChangeService(id, msg: any): Promise<any>{
+    const data = msg.split(' ');
+    const appointment = await appointments.findOne({
+      where: {
+        deleted: false,
+        client_id: id,
+        id: data[0]
+      }
+    });
+    if(data[1] = undefined)
+      data[1] = 'default'
+    await appointment.update({
+      service_id: data[1]
+    });
+    return 'Your appointment updated';
+  }
+
+  async ChangeDate(id, msg: any): Promise<any>{
+    const data = msg.split(' ');
+    const appointment = await appointments.findOne({
+      where: {
+        deleted: false,
+        client_id: id,
+        id: data[0]
+      }
+    });
+    if (data[1] == undefined)
+      data[1] = appointment.date;
+    const date = new Date(data[1]);
+    if(data[2] != undefined){
+    const time = data[2].split(':');
+    date.setHours(time[0]);
+    date.setMinutes(time[1]);}
+
+    await appointment.update({
+      date: date
+    });
+    return 'Your appointment updated';
+  }
+
+  async Set(id, msg: any): Promise<any> {
+    const data = msg.split(' ');
+    const date = new Date(data[0]);
+    const time = data[1].split(':');
+    date.setHours(time[0]);
+    date.setMinutes(time[1]);
+    try {
+      await appointments.create({
+        date: date,
+        barber_id: data[2],
+        service_id: data[3],
+        client_id: id,
+        deleted: false,
+      });
+      return 'Your appointment created successfully';
+    } catch (e) {
+      log_error(e)
+      console.log(e);
+      return 'Sorry, something wrong, we are working width it';
+    }
+  }
+}
+
+ /*
   async deleteAppointment(appointment: any): Promise<boolean> {
     //const appointment = await appointments.findOne({ where: { id: parseInt(should_be_appointment_id, 10) } });
     if (appointment != null) {
@@ -76,44 +135,7 @@ export class AppointmentRepository {
     return false;
   }
 
-  async setAppointment(
-    TelegramBot: any,
-    msg: any,
-    date: Date,
-    barber: any,
-    service: any,
-  ): Promise<any> {
-    try {
-      const appointment = await appointments.create({
-        // id по умолчанию
-        date: date,
-        barber_id: barber.id,
-        service_id: service.id,
-        client_id: msg.chat.id,
-        deleted: false,
-      });
-      if (appointment)
-        return await appointments.findOne({
-          where: {
-            id: appointment.id,
-          },
-          attributes: ['date', 'id'],
-          include: [
-            {
-              model: barbers,
-              attributes: ['first_name', 'last_name'],
-            },
-            {
-              model: services,
-              attributes: ['name'],
-            },
-          ],
-        });
-      else return false;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+
 
   async updateAppointment(currentAppointment) {
     const appointment = await appointments.findOne({
@@ -130,4 +152,17 @@ export class AppointmentRepository {
       deleted: currentAppointment.deleted,
     });
   }
-}
+
+  async GetAppointment(
+    appointment_id: number,
+    client_id: number,
+  ): Promise<any> {
+    return await appointments.findOne({
+      where: {
+        id: appointment_id,
+        client_id: client_id,
+        deleted: false,
+      },
+    });
+  }
+ */
--- src/service/AppointmentService.ts
+++ src/service/AppointmentService.ts
@@ -2,6 +2,7 @@ import { AppointmentRepository } from '../repositories/AppointmentRepository';
 const appointmentRepository = new AppointmentRepository();
 
 export class AppointmentService {
+
   async showMyAppointments(id: number): Promise<Array<any>> {
     return await appointmentRepository.showMyAppointments(id);
   }
@@ -10,41 +11,20 @@ export class AppointmentService {
     return await appointmentRepository.showMyHistory(id);
   }
 
-  async GetAppointment(
-    appointment_id: number,
-    client_id: number,
-  ): Promise<any> {
-    return await appointmentRepository.GetAppointment(
-      appointment_id,
-      client_id,
-    );
+  async Set(id, msg: any): Promise<any> {
+    return await appointmentRepository.Set(id, msg);
   }
 
-  async setAppointment(
-    TelegramBot: any,
-    msg: any,
-    date: Date,
-    barber: any,
-    service: any,
-  ): Promise<any> {
-    return await appointmentRepository.setAppointment(
-      TelegramBot,
-      msg,
-      date,
-      barber,
-      service,
-    );
+  async ChangeBarber(id, msg: any): Promise<any> {
+    return await appointmentRepository.ChangeBarber(id, msg);
   }
 
-  async deleteAppointment(
-    user_id: number,
-    should_be_appointment_id: number,
-  ): Promise<any> {
-    const appointment = appointmentRepository.GetAppointment(
-      should_be_appointment_id,
-      user_id,
-    );
-    return await appointmentRepository.deleteAppointment(appointment);
+  async ChangeService(id, msg: any): Promise<any> {
+    return await appointmentRepository.ChangeService(id, msg);
+  }
+
+  async ChangeDate(id, msg: any): Promise<any> {
+    return await appointmentRepository.ChangeDate(id, msg);
   }
 
   async freeDateAppointment(date: Date): Promise<string> {
@@ -60,42 +40,57 @@ export class AppointmentService {
     for (let i = 0; i < appointment.length; i++) {
       found_in_sec.push(
         appointment[i].date.getHours() * 3600 +
-          appointment[i].date.getMinutes() * 60,
+        appointment[i].date.getMinutes() * 60,
       );
     }
     found_in_sec = found_in_sec.sort();
     let R = 'We are free:\r\n';
     if (found_in_sec[0] != 36000)
-      R +=
-        'from 10:00 to ' +
-        found_in_sec[0] / 3600 +
-        ':' +
-        (found_in_sec[0] % 3600) / 60 +
-        '\r\n';
+      R += 'from 10:00 to ' +
+        found_in_sec[0] / 3600 + ':' +
+        (found_in_sec[0] % 3600) / 60 + '\r\n';
     for (let i = 0; i < found_in_sec.length - 1; i++) {
-      R +=
-        'from ' +
-        (found_in_sec[i] + 3600) / 3600 +
-        ':' +
-        ((found_in_sec[i] + 3600) % 3600) / 60 +
-        ' to ' +
-        found_in_sec[i + 1] / 3600 +
-        ':' +
-        (found_in_sec[i + 1] % 3600) / 60 +
-        '\r\n';
+      R += 'from ' +
+        (found_in_sec[i] + 3600) / 3600 + ':' +
+        ((found_in_sec[i] + 3600) % 3600) / 60 + ' to ' +
+        found_in_sec[i + 1] / 3600 + ':' +
+        (found_in_sec[i + 1] % 3600) / 60 + '\r\n';
     }
     if (found_in_sec[found_in_sec.length - 1] != 79200) {
-      R +=
-        'from ' +
-        found_in_sec[found_in_sec.length - 1] / 3600 +
-        ':' +
+      R += 'from ' +
+        found_in_sec[found_in_sec.length - 1] / 3600 + ':' +
         (found_in_sec[found_in_sec.length - 1] % 3600) / 60 +
         ' to 22:00\r\n';
     }
     return R;
   }
+}
+
+
+
+  /*
+  async GetAppointment(
+    appointment_id: number,
+    client_id: number,
+  ): Promise<any> {
+    return await appointmentRepository.GetAppointment(
+      appointment_id,
+      client_id,
+    );
+  }
+
+  async deleteAppointment(
+    user_id: number,
+    should_be_appointment_id: number,
+  ): Promise<any> {
+    const appointment = appointmentRepository.GetAppointment(
+      should_be_appointment_id,
+      user_id,
+    );
+    return await appointmentRepository.deleteAppointment(appointment);
+  }
 
   async updateAppointment(currentAppointment : any) : Promise<any> {
     await appointmentRepository.updateAppointment(currentAppointment);
   }
-}
+*/
