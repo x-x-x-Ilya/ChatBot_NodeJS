72f938988c86101471afeffa1184278833449b7f
Ilya Rypinski <iripinskij@gmail.com>
2020-07-15 20:56:44 +0300

    keyboard updated

--- src/keyboards/keyboards.ts
+++ src/keyboards/keyboards.ts
@@ -1,61 +1,32 @@
-import {
-  menuButtons,
-  profileButtons,
-  appointmentButtons,
-  editButtons,
-} from './key-board-buttons';
+import { isMenu, isAppointment, isEdit } from './keyboard-buttons';
 
 const menuKeyboard = [
-  [menuButtons.PriceList, menuButtons.BarberList],
-  [menuButtons.SignUpForAnAppointment], //, menuButtons.checkDateAppointment],
-  [menuButtons.MyProfile],
+  [isMenu.PriceList, isMenu.BarberList],
+  [isMenu.SignUpForAnAppointment, isMenu.Help],
+  [isMenu.Profile],
 ];
 
-const backKeyboard = [[menuButtons.Back]];
-
 const profileKeyboard = [
-  [profileButtons.Appointments, menuButtons.Back],
+  [isAppointment.History, isAppointment.Booked],
+  [isMenu.Help, isMenu.Back],
 ];
 
 const editKeyboard = [
-  [editButtons.ChangeDate, editButtons.ChangeBarber],
-  [editButtons.ChangeService, editButtons.ChangeTime],
-  [editButtons.Delete, menuButtons.Back],
+  [isEdit.ChangeDate, isEdit.ChangeBarber],
+  [isEdit.ChangeService, isEdit.ChangeTime],
+  [isEdit.Delete, isMenu.Back],
 ];
 
-const back_with_edit_keyboard = [[menuButtons.Back], [appointmentButtons.Edit]];
-
-function reply_markup(arg: string[][]) {
-  return JSON.stringify({
-    resize_keyboard: true,
-    keyboard: arg,
-  });
+function reply(arg: string[][]) {
+ return  {
+    reply_markup:
+      JSON.stringify({
+        resize_keyboard: true,
+        keyboard: arg,
+      }) }
 }
 
-export const back_with_edit_button = {
-  reply_markup: reply_markup(back_with_edit_keyboard),
-};
-export const menu = { reply_markup: reply_markup(menuKeyboard) };
-export const back = { reply_markup: reply_markup(backKeyboard) };
-export const help = { reply_markup: reply_markup(backKeyboard) };
-export const profile = { reply_markup: reply_markup(profileKeyboard) };
-export const edit = { reply_markup: reply_markup(editKeyboard) };
-
-export const appointment = {
-  reply_markup: {
-    inline_keyboard: [
-      [
-        {
-          text: appointmentButtons.AppointmentsHistory,
-          callback_data: 'appointmentsHistory',
-        },
-      ],
-      [
-        {
-          text: appointmentButtons.BookedAppointments,
-          callback_data: 'bookedAppointments',
-        },
-      ],
-    ],
-  },
-};
+export const menu = reply(menuKeyboard);
+export const help = reply(menuKeyboard);
+export const profile = reply(profileKeyboard);
+export const edit = reply(editKeyboard);
