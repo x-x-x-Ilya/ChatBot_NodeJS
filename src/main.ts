cd909265b7558999e7a03272bf8d5d7fa5d4c26a
Ilya Rypinski <iripinskij@gmail.com>
2020-07-15 20:56:56 +0300

    main cleaned

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import * as TelegramBot from 'node-telegram-bot-api';
 import { connect } from './database/synchronization';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const init = require('./database/models/index');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import fs from "fs";
+import { log_error } from './helpers/error-handler';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const init = require('./database/models/index');
 
 export const bot = new TelegramBot(process.env.TOKEN, {
-  webHook: {
-    port: 80
-  }
-});
+  webHook: { port: 80 } });
 
-// start ngrok http 80 for test on local machine
-const address = 'https://1d445b6a5401.ngrok.io/bot' + process.env.TOKEN;
+// start ngrok http 80       for test on local machine
+const address = 'https://5db5c9af78a5.ngrok.io/bot' + process.env.TOKEN;
 
 async function bootstrap() {
   connect().then(async () => {
@@ -21,14 +19,14 @@ async function bootstrap() {
       bot.setWebHook(address);
       bot.on('webhook_error', (error) => {
         console.log(error.code);
-        fs.appendFileSync('./logs/_errors.txt',JSON.stringify( error, null, '\t') + ' ' + new Date());
+        fs.appendFileSync('./logs/_errors.txt', JSON.stringify(error, null, '\t') + ' ' + new Date());
       });
       init.init();
       const app = await NestFactory.create(AppModule);
       await app.listen(80);
-    } catch (e) {
-      console.log(e);
-    }
+    } catch (error) {
+    log_error(error);
+  }
   });
 }
 bootstrap();
