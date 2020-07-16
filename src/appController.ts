181da90fb38828b934bbacdcbfa78e593a0cc376
Ilya Rypinski <iripinskij@gmail.com>
2020-07-16 13:42:30 +0300

    komands description updated

--- src/appController.ts
+++ src/appController.ts
@@ -2,7 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { bot } from './main';
 import { menu, help, profile } from './keyboards/keyboards';
 import { isAppointment, isEdit, isMenu } from './keyboards/keyboard-buttons';
-import { helpText } from './helpers/helpText';
+import {
+  beditText,
+  checkText,
+  deditText,
+  deleteText,
+  helpText,
+  lText,
+  mText,
+  seditText,
+  signText,
+} from './helpers/helpText';
 import { sendMessage } from './helpers/sendMessage';
 import { routes } from './route';
 import { log } from './helpers/logUserMessage';
@@ -14,16 +24,22 @@ export class appController {
   async onMessage(@Body() update): Promise<void> {
     console.log(update);
     log(update);
-
-    if(update.message.text == '/l' ||
-      update.message.text == '/m' ||
-      update.message.text == '/check' ||
-      update.message.text == '/sign' ||
-      update.message.text == '/bedit' ||
-      update.message.text == '/sedit' ||
-      update.message.text == '/dedit')
-      sendMessage(bot, update.message, helpText, help);
-
+    if (update.message.text === '/l')
+      sendMessage(bot, update.message, await routes.client.Profile(update.message) + lText, help);
+    else if (update.message.text === '/sedit')
+      sendMessage(bot, update.message, await routes.appointment.Booked(update.message) + '\n' + await routes.service.List() + seditText, help);
+    else if (update.message.text === '/m')
+      sendMessage(bot, update.message, await routes.client.Profile(update.message) + mText, help);
+    else if (update.message.text === '/check')
+      sendMessage(bot, update.message, checkText, help);
+    else if (update.message.text === '/sign')
+      sendMessage(bot, update.message, signText, help);
+    else if (update.message.text === '/dedit')
+      sendMessage(bot, update.message, await routes.appointment.Booked(update.message) + deditText, help);
+    else if (update.message.text === '/delete')
+      sendMessage(bot, update.message, await routes.appointment.Booked(update.message) + deleteText, help);
+    else if (update.message.text === '/bedit')
+      sendMessage(bot, update.message, await routes.appointment.Booked(update.message) + '\n' + await routes.barber.List() + beditText, menu);
     else if (update.message.text.indexOf('/l') != -1)
       sendMessage(bot, update.message, await routes.client.SetLastName(update.message.text.substring(3), update.message.chat.id), profile);
     else if (update.message.text.indexOf('/m') != -1)
@@ -38,6 +54,8 @@ export class appController {
       sendMessage(bot, update.message, await routes.appointment.ChangeService(update.message.chat.id, update.message.text.substring(7)), menu);
     else if (update.message.text.indexOf('/dedit') != -1)
       sendMessage(bot, update.message, await routes.appointment.ChangeDate(update.message.chat.id, update.message.text.substring(7)), menu);
+    else if (update.message.text.indexOf('/delete') != -1)
+      sendMessage(bot, update.message, await routes.appointment.Delete(update.message.chat.id, update.message.text.substring(8)), menu);
     else if (update.message.text == isMenu.Back)
       sendMessage(bot, update.message, helpText, menu);
     else if (update.message.text == isMenu.BarberList)
@@ -57,78 +75,3 @@ export class appController {
       sendMessage(bot, update.message, helpText, help);
   }
 }
-
-
-/*
-    else if (update.message.text == isEdit.ChangeTime)
-      sendMessage(bot, update.message, 'no code now(', menu);
-else if (update.message.text == editButtons.ChangeBarber) {
-      set_barber_message_id = update.message.message_id + 3;
-      await barberRouter.BarberList(bot, update.message);
-      bot.sendMessage(update.message.chat.id,'Enter barber id you want',back);
-    }
-    else if (update.message.text == editButtons.ChangeService) {
-      set_service_msg_id = update.message.message_id + 3;
-      await serviceRouter.PriceList(bot, update.message);
-      bot.sendMessage(update.message.chat.id, 'Enter service id you want', back);
-    }
-    else if (update.message.text == menuButtons.SignUpForAnAppointment) {
-        isCreating = true;
-        sign_up_for_appointment_message = update.message.chat.id + 2;
-        bot.sendMessage(update.message.chat.id,'Enter date you would like to visit us (format: "06.05.2020")',back);
-    }
-else if (update.message.text == appointmentButtons.Edit) {
-    edit_appointment_message_id = update.message.message_id + 2;
-    bot.sendMessage(update.message.chat.id,'Enter appointment id you would like to edit',back);
-  }
-else if (update.message.message_id == set_barber_message_id) {
-     barber = await barberRouter.SetBarber(bot, update.message);
-     if (isCreating) {
-       await serviceRouter.PriceList(bot, update.message);
-       set_service_msg_id = update.message.message_id + 2;
-       bot.sendMessage(update.message.chat.id, 'Select id service you want'back);
-     }
-     if (!isCreating) {
-       cur_appointment.barber_id = barber.id;
-       await appointmentRouter.updateAppointment(cur_appointment);
-     }
-   } else if (update.message.message_id == sign_up_for_appointment_message) {
-     date = await appointmentRouter.SetDate(bot, update.message);
-     if (isCreating) {
-       set_time_message_id = update.message.message_id + 2;
-       bot.sendMessage(update.message.chat.id, 'Enter time, you would like to visit us (format: "16:00")', back);
-     }
-   } else if (update.message.message_id == set_time_message_id) {
-     await appointmentRouter.SetTime(bot, update.message, date);
-     if (isCreating) {
-       await barberRouter.BarberList(bot, update.message);
-       set_barber_message_id = update.message.message_id + 2;
-       bot.sendMessage(
-         update.message.chat.id,
-         'Enter barber id you want',
-         back,
-       );
-     }
-   } else if (update.message.message_id == edit_appointment_message_id) {
-     cur_appointment = await appointmentRouter.GetAppointment(
-       update.message,
-       parseInt(update.message.text, 10),
-     );
-     if (cur_appointment != null)
-       bot.sendMessage(update.message.chat.id, 'Select operation', edit);
-     else
-       bot.sendMessage(
-         update.message.chat.id,
-         'Error, please try again...',
-         back,
-       );
-   } else if (update.message.message_id == set_service_msg_id) {
-     const service = await serviceRouter.SetService(bot, update.message);
-   }
-
-else if (update.message.text == editButtons.Delete) {
-  if (await appointmentRouter.RemoveMyAppointment(bot, update.message, cur_appointment.id))
-    sendMessage(bot, update.message, 'Appointment removed successfully.', back);
-  else
-    sendMessage(bot, update.message, 'Operation error, please, try again.', back);
-}*/
--- src/helpers/helpText.ts
+++ src/helpers/helpText.ts
@@ -1,10 +1,27 @@
 export const helpText =
   'My commands:\n' +
-  '/l - new last name (set\'s your last name) for example: “/l Putin”\n' +
-  '/m - new email (set\'s your email) for example: “/m examplemail@gmail.com”\n' +
-  '/help - output all commands for example: “/help”\n' +
-  '/check - 00.00.0000 (check’s free time this date) for example: “/check 01.01.2022”\n' +
-  '/sign - dd.mm.yyyy hh:mm barberID serviceID (set’s your new appointment) for example: “/sign 01.01.2001 15:00 4 5”\n' +
-  '/dedit - AppointmentID dd.mm.yyyy hh:mm (change date && time) for example: “/dedit 1 01.01.2022 15:00”\n' +
-  '/bedit - AppointmentID barberID (change barber) for example: “/bedit 1 1”\n' +
-  '/sedit - AppointmentID serviceID (change service) for example: “/sedit 1 1”\n';
+  '/l new last name (set\'s your last name) for example: “/l Putin”\n' +
+  '/m new email (set\'s your email) for example: “/m examplemail@gmail.com”\n' +
+  '/help output all commands for example: “/help”\n' +
+  '/check 00.00.0000 (check’s free time this date) for example: “/check 01.01.2022”\n' +
+  '/sign dd.mm.yyyy hh:mm barberID serviceID (set’s your new appointment) for example: “/sign 01.01.2001 15:00 4 5”\n' +
+  '/dedit AppointmentID dd.mm.yyyy hh:mm (change date && time) for example: “/dedit 1 01.01.2022 15:00”\n' +
+  '/bedit AppointmentID barberID (change barber) for example: “/bedit 1 1”\n' +
+  '/sedit AppointmentID serviceID (change service) for example: “/sedit 1 1”\n' +
+  '/delete AppointmentID (delete appointment) for example: “/delete 1”';
+
+export const beditText = '\n/bedit AppointmentID barberID (change barber) for example: “/bedit 1 1”\n';
+
+export const deleteText = '\n/delete AppointmentID (delete appointment) for example: “/delete 1”';
+
+export const deditText = '\n/dedit AppointmentID dd.mm.yyyy hh:mm (change date && time) for example: “/dedit 1 01.01.2022 15:00”\n'
+
+export const seditText = '\n/sedit AppointmentID serviceID (change service) for example: “/sedit 1 1”\n';
+
+export const signText = '\n/sign dd.mm.yyyy hh:mm barberID serviceID (set’s your new appointment) for example: “/sign 01.01.2001 15:00 4 5”\n';
+
+export const checkText = '\n/check 00.00.0000 (check’s free time this date) for example: “/check 01.01.2022”\n';
+
+export const mText = '\n/m new email (set\'s your email) for example: “/m examplemail@gmail.com”\n';
+
+export const lText = '\n/l new last name (set\'s your last name) for example: “/l Putin”\n';
