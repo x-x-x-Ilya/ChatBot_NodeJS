9c00f4014c5797e034b9c8d9da1df76eb3b91e9e
Ilya <iripinskij@gmail.com>
2020-07-20 20:13:51 +0300

    all log functions replaced to one file

---
+++ src/middleware/logging.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs'; // Node.js module for working with files in Node.js
+
+// for logging user messages
+export const log_user = (update : any)  => {
+  fs.appendFileSync('./logs/' + update.message.chat.id + '.txt', '\n user: ' + JSON.stringify(update.message.text, null, '\t') + ' ' + new Date());
+}
+
+// for logging errors
+export function log_error(error : string | JSON) : any {
+  console.log(error);
+  fs.appendFileSync('./logs/_errors.txt', JSON.stringify(error, null, '\t') + ' ' + new Date());
+}
+
+// for logging bot messages
+export const log_bot = (id, text: any)  => {
+  fs.appendFileSync(`./logs/` + id + `.txt`, '\n bot: ' + JSON.stringify(text, null, '\t') + ' ' + new Date());
+}
