20b4b8a5d26a56a88d5a3e0104c275a8f87d1091
Ilya <iripinskij@gmail.com>
2020-07-21 16:44:20 +0300

    send message function optimized

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs'; // Node.js module for working with files in Node.js
 
 // for logging user messages
 export const log_user = (update : any)  => {
+  console.log(update);
   fs.appendFileSync('./logs/' + update.message.chat.id + '.txt', '\n user: ' + JSON.stringify(update.message.text, null, '\t') + ' ' + new Date());
 }
 
@@ -15,3 +16,6 @@ export function log_error(error : string | JSON) : any {
 export const log_bot = (id, text: any)  => {
   fs.appendFileSync(`./logs/` + id + `.txt`, '\n bot: ' + JSON.stringify(text, null, '\t') + ' ' + new Date());
 }
+
+
+
--- src/middleware/sendMessage.ts
+++ src/middleware/sendMessage.ts
@@ -1,9 +1,9 @@
 import { log_bot, log_error } from './logging';
+import { bot } from '../main';
 
-export const sendMessage = (TelegramBot : any, msg : any, text : any, keyboard : any) : void => {
-
-  TelegramBot.sendMessage(msg.chat.id, text, keyboard).catch(error => {
-    log_bot(msg.chat.id, text);
+export const send = (id : number, text : string, keyboard : any) : void => {
+  bot.sendMessage(id, text, keyboard).catch(error => {
+    log_bot(id, text);
     log_error(error);
   });
 };
