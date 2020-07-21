9e673e170ca5c5007ed45cc7cb775ec3b5c9d94c
Ilya <iripinskij@gmail.com>
2020-07-21 16:43:32 +0300

    dependences added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';  // Node.js framework for building efficient and scalable server-side applications.
 import { appController } from './appController';
 
+
+import { ClientController} from './controller/ClientController';
+import { AppointmentController} from './controller/AppointmentController';
+import { ServiceController} from './controller/ServiceController';
+import { BarberController} from './controller/BarberController';
+
 // Root module, the starting point that Nest uses to build the application graph
 @Module({
   imports: [],
   controllers: [appController],
-  providers: [],
+  providers: [BarberController, ServiceController, AppointmentController, ClientController],
+  exports: [BarberController, ServiceController, AppointmentController, ClientController]
 })
-export class AppModule {
-}
+
+export class AppModule {}
