f8e77e93a4a8d9d4b98933613502ba0ccd20c970
Ilya <iripinskij@gmail.com>
2020-08-05 16:58:31 +0300

    back up upgraded typed

--- src/database/backup.ts
+++ src/database/backup.ts
@@ -4,12 +4,13 @@ const exec = require('child_process').exec;
 const CronJob = require('cron').CronJob;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
-
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
 const autoBackupPath = 'C:\\project\\ChatBot_NodeJS\\back\\PostgresSQL.sql';
 const dir = 'C:\\project\\ChatBot_NodeJS\\back';
 
-
 const dbAutoBackUp = (): void => {
+
   fs.readdir(dir, (err, files) => {
     // delete old files
     while (files.length >= 4) {
@@ -21,89 +22,137 @@ const dbAutoBackUp = (): void => {
       });
     }
   });
+
   // create new back up file
   const date = new Date();
-  const newBackupDir =
+  const newBackup =
     date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-  // New backup path for current backup process
-  const newBackupPath = autoBackupPath + 'psql-' + newBackupDir + '.sql';
+  const newBackupPath = autoBackupPath + 'psql-' + newBackup + '.sql';
+
   const cmd = 'pg_dump --no-password -U' + process.env.USER_NAME + ' ' +
     process.env.DB_NAME + ' > ' + newBackupPath;
+
   exec(cmd, { cwd: 'C:\\Program Files\\PostgreSQL\\12\\bin\\' });
+
   //copy to mongo
+
   mongoInit(newBackupPath);
 }
 
-export const job = new CronJob('0 0 0 * * 0', dbAutoBackUp(), null, true);
-
 function mongoInit(path) {
-  fs.readFile(path, "utf8", function(error, data) {
+  fs.readFile(path, "utf8",
+    function(error, data) {
       if (error) {
         logError(error);
         throw error;
       }
+
       // все объекты
       data = data.substring(data.indexOf('COPY'));
       data = data.substring(0, data.lastIndexOf('\\.') + 2);
+
       let collections;
       while (data.indexOf('COPY') != -1) {
+
         collections = data.substring(  // объекты одной таблицы
           data.indexOf('COPY'),
           data.indexOf('\\.') + 2);
+
         data = data.replace(collections, '');
+
         const fields_str = collections.substring(
           collections.indexOf('(') + 1,
           collections.indexOf(')'));
+
         const fields = fields_str.split(', ');   // поля таблицы
+
         // имя таблицы
         let collection_name = collections.substring(
           collections.indexOf('COPY public.') + 'COPY public.'.length,
           collections.indexOf('(') - 1)
+
         // значения таблицы
         let values: string = collections.substring(
           collections.indexOf('FROM stdin;') + 'FROM stdin;'.length + 2,
           collections.indexOf('\\.'));
+
         let contenet = '[\n';
         while (values.indexOf('\n') != -1 && values.length != 0) {
+
           let line_values = values.substring(0, values.indexOf('\n') + 1);
           values = values.replace(line_values, '');
           line_values = line_values.substring(0, line_values.length - 2);
+
           const arr_values = line_values.split('\t');
-          for (let j = 0; j < arr_values.length; j++) {
-            if(arr_values[j] == 'f') arr_values[j] = 'false';
-            if(arr_values[j] == 't') arr_values[j] = 'true';
-            if(arr_values[j] == '\\N') arr_values[j] = 'null';
-          }
-          for (let j = 0; j < arr_values.length; j++) {
-            console.log(arr_values[j]);
-          }
 
           contenet += '\t{\n';
+          let _;
           for (let j = 0; j < arr_values.length; j++) {
+            _ = '"';
+
+            /*if(isNum(Date.parse(arr_values[j])))
+              _ = '';*/
+
+            if(isNum(arr_values[j]))
+              _ = '';
+
+            if(arr_values[j] == 'f') {
+              arr_values[j] = 'false';
+              _ = '';
+            }
+
+            if(arr_values[j] == 't') {
+              arr_values[j] = 'true';
+              _ = '';
+            }
+
+            if(arr_values[j] == '\\N') {
+              arr_values[j] = 'null';
+              _ = '';
+            }
+
             if (j != fields.length - 1)
-              contenet += '\t\t' + fields[j] + ': "' + arr_values[j] + '",\n';
+              contenet += '\t\t' + fields[j] + ': ' + _ + arr_values[j] + _ + ',\n';
             else
-              contenet += '\t\t' + fields[j] + ': "' + arr_values[j] + '"\n\t},\n';
+              contenet += '\t\t' + fields[j] + ': ' + _ + arr_values[j] + _ + '\n\t},\n';
           }
         }
+
         contenet = contenet.substring(0, contenet.length - 2);
         if (contenet.indexOf('[') != -1)
           contenet += '\n]';
-        else
-          contenet += '[{}]'
-
+        else {
+          contenet += '[{ date: "2020-05-06T09:00:00.000+00:00" }]'
+        }
         while (collection_name.indexOf('"') != -1)
           collection_name = collection_name.replace('"', '');
 
-         fs.writeFile("D:\\mongotest\\" + collection_name + ".json", contenet + " ",
+         fs.writeFile("D:\\mongotest\\" + collection_name + ".json", contenet,
            function(error) {
           if (error) {
             logError(error);
             throw error;
           }
 
-          // копирует данные из сформированного json файла
-          const cmd = "mongoimport --db test --collection " + collection_name + " < D:/mongotest/" + collection_name + ".json --legacy --jsonArray";
+             // удаляет старые данные
+             MongoClient.connect(url, function(err, db) {
+               if (err) throw err;
+               const dbo = db.db("test");
+               try {
+                 dbo.collection(collection_name).drop(function(err, delOK) {
+                   if (err) throw err;
+                   if (delOK) console.log("Collection deleted");
+                   db.close();
+                 });
+               } catch (e) {
+                 logError(e);
+               }
+             });
+
+             // копирует данные из сформированного json файла
+          const cmd = "mongoimport --db test --collection " +
+            collection_name + " < D:/mongotest/" + collection_name + ".json" +
+            " --legacy --jsonArray";
           exec(cmd, { cwd: 'C:\\Program Files\\MongoDB\\Server\\4.2\\bin' },
             function(error) {
             if (error) {
@@ -114,6 +163,17 @@ function mongoInit(path) {
 
         });
       }
-
     });
 }
+
+function isNum(num){
+  return !isNaN(num)
+}
+
+dbAutoBackUp();
+//export const job = new CronJob('0 0 0 * * 0', dbAutoBackUp(), null, true);
+
+
+
+
+
