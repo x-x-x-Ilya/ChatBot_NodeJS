4218bb97a7978f608d81659be5fdd193a7fe44c4
Ilya <iripinskij@gmail.com>
2020-08-07 20:16:08 +0300

    code replaced to services

--- src/controller/ServiceController.ts
+++ src/controller/ServiceController.ts
@@ -4,27 +4,11 @@ const serviceService = new ServiceService();
 export class ServiceController {
 
   async amenitiesList(): Promise<string> {
-    const services: Array<any> = await serviceService.amenitiesList();
-    if (services.length == 0) return 'There are no services, write me later';
-      let Response = '\r\n';
-      for (let i = 0; i < services.length; i++) {
-        Response += '[' + services[i].id + '] ' + services[i].name + '\t' +
-          services[i].time + '\t' + services[i].price.toString() + '\r\n';
-      }
-      return Response;
+    return  await serviceService.amenitiesList();
   }
 
   async barberList(): Promise<string> {
-    const barbers: Array<any> = await serviceService.barberList();
-    if(barbers.length == 0)
-      return 'There are no barbers, please write me later';
-
-    let Response = '\r\n';
-    for (let i = 0; i < barbers.length; i++) {
-      Response += '[' + barbers[i].id + '] ' + barbers[i].first_name + ' '
-        + barbers[i].last_name + '\r\n';
-    }
-    return Response;
+    return await serviceService.barberList();
   }
 
 
--- src/service/ServiceService.ts
+++ src/service/ServiceService.ts
@@ -3,11 +3,27 @@ const serviceRepository = new ServiceRepository();
 
 export class ServiceService {
 
-  async amenitiesList(): Promise<any> {
-    return await serviceRepository.amenitiesList();
+  async amenitiesList(): Promise<string> {
+    const services: Array<any> = await serviceRepository.amenitiesList();
+    if (services.length == 0)
+      return 'There are no services, write me later';
+    let Response = '\r\n';
+    for (let i = 0; i < services.length; i++) {
+      Response += '[' + services[i].id + '] ' + services[i].name + '\t' +
+        services[i].time + '\t' + services[i].price.toString() + '\r\n';
+    }
+    return Response;
   }
 
-  async barberList(): Promise<any> {
-    return await serviceRepository.barberList();
+  async barberList(): Promise<string> {
+    const barbers: Array<any> = await serviceRepository.barberList();
+    if(barbers.length == 0)
+      return 'There are no barbers, please write me later';
+    let Response = '\r\n';
+    for (let i = 0; i < barbers.length; i++) {
+      Response += '[' + barbers[i].id + '] ' + barbers[i].first_name + ' '
+        + barbers[i].last_name + '\r\n';
+    }
+    return Response;
   }
 }
