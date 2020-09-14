30315bfc1776797ea33177f178ba4dec82489c41
Ilya <iripinskij@gmail.com>
2020-09-14 14:13:21 +0300

    logs with firebase

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -1,11 +1,13 @@
 import * as fs from 'fs';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const functions = require("firebase-functions");
 
 // universal function for logging all type errors
 export const log = (file: string, text: any, author: string): void => {
-  if(author === ' ') console.log(text);
   fs.appendFileSync(file,
     '\n' + author + ': ' +
     JSON.stringify(text, null, '\t') + ' ' + new Date());
+  functions.logger.log(JSON.stringify(text, null, '\t'));
 }
 /** For logging:
   * User messages: log('./logs/' + id + '.txt', text, 'user');
