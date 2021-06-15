bd4c45eac910f509ec545e1fd962510632818a09
Ilya Ripinski <iripinskij@gmail.com>
2021-06-16 00:51:50 +0300

    feat(errorHandler): constants added

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const address = 'localhost';
 export const port = 80;
 export const errLogPath = './logs/_errors.txt';
+export const webhook_error = 'webhook_error';
--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,16 +1,17 @@
 import * as TelegramBot from 'node-telegram-bot-api';
 import { log } from './logging';
+import { webhook_error, errLogPath } from '../constants';
 
 // listener for bot errors
 export async function onError(bot: TelegramBot): Promise<void> {
     try {
-        bot.on('webhook_error', error => {
-            log('./logs/_errors.txt', error + '\n code: ' + error.code, ' ');
+        bot.on(webhook_error, error => {
+            log(errLogPath, error + '\n code: ' + error.code, ' ');
         });
         bot.on('error', e => {
-            log('./logs/_errors.txt', e, ' ');
+            log(errLogPath, e, ' ');
         });
     } catch (e) {
-        log('./logs/_errors.txt', e, ' ');
+        log(errLogPath, e, ' ');
     }
 }
